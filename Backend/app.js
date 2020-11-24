6015b7c3acd83ea8e16ae8c24720d55597e12922
rakshitha-sathyakumar <rakshu595@gmail.com>
2020-11-23 16:07:45 -0800

    modified mongo db models

--- Backend/app.js
+++ Backend/app.js
@@ -12,11 +12,11 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(passport.initialize());
-app.use(cors({ origin: 'http://3.131.251.52:3000', credentials: true }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://3.131.251.52:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
